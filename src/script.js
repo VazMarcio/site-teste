7c1bcb98c4b3668ef77f5f0e772314f009141274
VazMarcio <githubmarcio@gmail.com>
2025-10-26 18:43:00 -0300

    Atualizando index.html e script.js

--- src/script.js
+++ src/script.js
@@ -2,3 +2,22 @@ function mostrarAlerta() {
   const mensagem = document.getElementById("mensagem");
   mensagem.textContent = "Você clicou no botão! 🎉";
 }
+function mostrarAlerta() {
+  const mensagem = document.getElementById("mensagem");
+  mensagem.textContent = "Você clicou no botão! 🎉";
+}
+
+function mostrarData() {
+  const info = document.getElementById("info");
+  const dataAtual = new Date().toLocaleDateString("pt-BR");
+  info.textContent = `Data atual: ${dataAtual}`;
+}
+
+function mudarCor() {
+  document.body.style.backgroundColor = "#BFEFFF"; // azul bebê
+}
+
+function mostrarMensagem() {
+  const info = document.getElementById("info");
+  info.textContent = "Você desbloqueou uma mensagem secreta! ✨";
+}
